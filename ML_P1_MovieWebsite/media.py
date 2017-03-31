23a6eba54eef872b6ca0c91ffa0393795d816b4e
gdefenser <qier_241078450@qq.com>
2017-03-31 16:23:57 +0800

    Modify movie website

--- ML_P1_MovieWebsite/media.py
+++ ML_P1_MovieWebsite/media.py
@@ -1,12 +1,15 @@
 import webbrowser
 
-# defind class Movie
+# Defind class Movie
 class Movie():
-    # movie parameters
+    """ Constructor for init movie parameters
+        Paras : pointer,movie attributes"""
     def __init__(self,movie_title,poster_image,tralier_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = tralier_youtube
 
+    """ Show current movie instance's trailer in browser
+        Paras : pointer"""
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
