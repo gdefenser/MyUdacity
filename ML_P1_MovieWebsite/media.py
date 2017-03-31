ad276e733843eb675724b48d9f5ce265b552f78c
gdefenser <qier_241078450@qq.com>
2017-03-31 14:37:21 +0800

    Modify movie website

--- ML_P1_MovieWebsite/media.py
+++ ML_P1_MovieWebsite/media.py
@@ -3,9 +3,8 @@ import webbrowser
 # defind class Movie
 class Movie():
     # movie parameters
-    def __init__(self,movie_title,movie_stroyline,poster_image,tralier_youtube):
+    def __init__(self,movie_title,poster_image,tralier_youtube):
         self.title = movie_title
-        self.stroyline = movie_stroyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = tralier_youtube
 
