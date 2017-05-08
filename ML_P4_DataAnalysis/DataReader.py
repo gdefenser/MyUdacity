2fb77c49be2ce5955bb6f6636253c08e387c744f
gdefenser <qier_241078450@qq.com>
2017-05-08 23:35:19 +0800

    ML_P4

---
+++ ML_P4_DataAnalysis/DataReader.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+
+folder_path = '/home/loveshadev/PycharmProjects/Udacity/ML_P4_DataAnalysis/'
+
+#filter Data set
+def filterDfByLastTenYears(df):
+    return df[(df['yearID'] >= 2016) & (df['yearID'] <= 2017)]
+
+#read data-set of All star
+df_allstar = filterDfByLastTenYears(pd.read_csv(folder_path+'AllstarFull.csv'))
+#read data-set of Batting
+df_batting = filterDfByLastTenYears(pd.read_csv(folder_path+'Batting.csv'))
+#read data-set of WSWin Teams
+df_teams = filterDfByLastTenYears(pd.read_csv(folder_path+'Teams.csv'))
+df_teams = df_teams[df_teams['WSWin']=='Y']
+
+
+df_merged_teams_batting = df_teams.merge(df_batting,on=['teamID'],how='inner')
+df_merged_allstar_batting = df_allstar.merge(df_batting,on=['playerID'],how='left')
+#print len(df_allstar)
+#print len(df_batting)
+#print len(df_teams)
+
+print len(df_merged_teams_batting)
+print len(df_merged_allstar_batting)
--- TestProject/DataAnalysis/ML_L63_test.py
+++ TestProject/DataAnalysis/ML_L63_test.py
@@ -1,81 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import seaborn as sns
 
-# Adding using +
+values = np.array([1, 3, 2, 4, 1, 6, 4])
+example_df = pd.DataFrame({
+    'value': values,
+    'even': values % 2 == 0,
+    'above_three': values > 3
+}, index=['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+
+# Change False to True for this block of code to see what it does
+
+# groupby() without as_index
 if False:
-    s = pd.Series([1, 2, 3, 4])
-    df = pd.DataFrame({
-        0: [10, 20, 30, 40],
-        1: [50, 60, 70, 80],
-        2: [90, 100, 110, 120],
-        3: [130, 140, 150, 160]
-    })
+    first_even = example_df.groupby('even').first()
+    print first_even
+    print first_even['even']  # Causes an error. 'even' is no longer a column in the DataFrame
 
-    print df
-    print ''  # Create a blank line between outputs
-    print df + s
-
-# Adding with axis='index'
+# groupby() with as_index=False
 if False:
-    s = pd.Series([1, 2, 3, 4])
-    df = pd.DataFrame({
-        0: [10, 20, 30, 40],
-        1: [50, 60, 70, 80],
-        2: [90, 100, 110, 120],
-        3: [130, 140, 150, 160]
-    })
+    first_even = example_df.groupby('even', as_index=False).first()
+    print first_even
+    print first_even['even']  # Now 'even' is still a column in the DataFrame
 
-    print df
-    print ''  # Create a blank line between outputs
-    print df.add(s, axis='index')
-    # The functions sub(), mul(), and div() work similarly to add()
+filename = '/home/loveshadev/PycharmProjects/Udacity/TestProject/DataAnalysis/nyc-subway-weather.csv'
+subway_df = pd.read_csv(filename)
 
-# Adding with axis='columns'
-if False:
-    s = pd.Series([1, 2, 3, 4])
-    df = pd.DataFrame({
-        0: [10, 20, 30, 40],
-        1: [50, 60, 70, 80],
-        2: [90, 100, 110, 120],
-        3: [130, 140, 150, 160]
-    })
-
-    print df
-    print ''  # Create a blank line between outputs
-    print df.add(s, axis='columns')
-    # The functions sub(), mul(), and div() work similarly to add()
-
-grades_df = pd.DataFrame(
-    data={'exam1': [43, 81, 78, 75, 89, 70, 91, 65, 98, 87],
-          'exam2': [24, 63, 56, 56, 67, 51, 79, 46, 72, 60]},
-    index=['Andre', 'Barry', 'Chris', 'Dan', 'Emilio',
-           'Fred', 'Greta', 'Humbert', 'Ivan', 'James']
-)
-
-
-def standardize(df):
-    '''
-    Fill in this function to standardize each column of the given
-    DataFrame. To standardize a variable, convert each value to the
-    number of standard deviations it is above or below the mean.
-
-    This time, try to use vectorized operations instead of apply().
-    You should get the same results as you did before.
-    '''
-    mean = df.mean()
-    std = df.std()
-    return (df - mean)/std
-
-
-def standardize_rows(df):
-    '''
-    Optional: Fill in this function to standardize each row of the given
-    DataFrame. Again, try not to use apply().
-
-    This one is more challenging than standardizing each column!
-    '''
-
-    mean = grades_df.mean()
-    std = grades_df.std()
-    return mean
-
-print standardize(grades_df)
+## Make a plot of your choice here showing something interesting about the subway data.
+## Matplotlib documentation here: http://matplotlib.org/api/pyplot_api.html
+## Once you've got something you're happy with, share it on the forums!
+group_by_localization = subway_df.groupby(['latitude','longitude'], as_index=False).mean()
+plt.scatter(group_by_localization['latitude'],group_by_localization['longitude'],
+            s=group_by_localization['ENTRIESn_hourly'])
