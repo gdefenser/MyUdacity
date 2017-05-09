b843dc740d18f5e5ddf8de615cca94f1deccc1ca
gdefenser <qier_241078450@qq.com>
2017-05-09 22:14:13 +0800

    ML_P4

--- ML_P4_DataAnalysis/DataReader.py
+++ ML_P4_DataAnalysis/DataReader.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 folder_path = '/home/loveshadev/PycharmProjects/Udacity/ML_P4_DataAnalysis/'
 
 #filter Data set
 def filterDfByLastTenYears(df):
-    return df[(df['yearID'] >= 2016) & (df['yearID'] <= 2017)]
+    return df[(df['yearID'] >= 2007) & (df['yearID'] <= 2017)]
 
 #read data-set of All star
 df_allstar = filterDfByLastTenYears(pd.read_csv(folder_path+'AllstarFull.csv'))
@@ -16,12 +16,33 @@ df_batting = filterDfByLastTenYears(pd.read_csv(folder_path+'Batting.csv'))
 df_teams = filterDfByLastTenYears(pd.read_csv(folder_path+'Teams.csv'))
 #df_teams = df_teams[df_teams['WSWin']=='Y']
 
-
-df_merged_teams_batting = df_teams.merge(df_batting,on=['teamID'],how='inner')
-df_merged_allstar_batting = df_allstar.merge(df_batting,on=['playerID'],how='left')
 #print len(df_allstar)
 #print len(df_batting)
 #print len(df_teams)
 
-#print len(df_merged_teams_batting)
-#print len(df_merged_allstar_batting)
+def printDiscrtption(df):
+    print 'Minimum value by each year'
+    print df.min()
+    print '============================================'
+    print 'Minimum value by each year'
+    print df.max()
+    print '============================================'
+    print 'Mean value by each year'
+    print df.mean()
+    print '============================================'
+    print 'Standard deviation by each year'
+    print df.std()
+
+
+def drawGrid(x1_set,y1_set,x2_set,y2_set,label_1,label_2):
+    plt.plot(x1_set, y1_set, 'r', label=label_1)
+    plt.plot(x2_set, y2_set, 'b', label=label_2)
+    plt.legend(bbox_to_anchor=[0.4, 1])
+    plt.grid()
+
+def calculateWinRate(df_teams_row):
+    return float(df_teams_row.W) / float(df_teams_row.W+df_teams_row.L)
+
+df_analysis_allstar_batting = df_allstar.merge(df_batting,on=['yearID','teamID','playerID'],how='inner')
+
+print len(df_analysis_allstar_batting)
