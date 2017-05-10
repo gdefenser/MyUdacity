d6f71600f2dff465746bace9af6726a08f874993
gdefenser <qier_241078450@qq.com>
2017-05-10 22:25:52 +0800

    ML_P4

--- ML_P4_DataAnalysis/DataReader.py
+++ ML_P4_DataAnalysis/DataReader.py
@@ -43,6 +43,35 @@ def drawGrid(x1_set,y1_set,x2_set,y2_set,label_1,label_2):
 def calculateWinRate(df_teams_row):
     return float(df_teams_row.W) / float(df_teams_row.W+df_teams_row.L)
 
-df_analysis_allstar_batting = df_allstar.merge(df_batting,on=['yearID','teamID','playerID'],how='inner')
+def calculateHitRate(df_lan_batting_row):
+    if df_lan_batting_row.AB == 0:
+        return float(0)
+    else:
+        return float(df_lan_batting_row.H) / float(df_lan_batting_row.AB)
 
-print len(df_analysis_allstar_batting)
+df_batting['HitRate'] = df_batting.apply(calculateHitRate,axis=1)
+df_lan_batting = df_batting[df_batting['teamID']=='LAN']
+df_lan_allstar_batting = df_allstar.merge(df_lan_batting,on=['yearID','teamID','playerID'],how='inner')
+
+
+df_analysis_lan_batting = df_lan_batting.groupby(['yearID'])['yearID','H','AB','HitRate','HR','SO','SB']
+df_analysis_lan_allstar_batting = df_lan_allstar_batting.groupby(['yearID'])[['yearID','H','AB','HitRate','HR','SO','SB']]
+
+#var = df.groupby(['BMI', 'Gender']).Sales.sum()
+#var.unstack().plot(kind='bar', stacked=True, color=['red', 'blue'])
+#var.unstack().plot(kind='bar', stacked=True, color=['red', 'blue'])
+#plt.show()
+def bulidYearRdsDf(df_lan_allstar_batting,df_analysis_lan_allstar_batting):
+    df_year_rds = pd.DataFrame(data={
+            'all_players':df_analysis_lan_batting.count()['yearID'],
+            'all_star_players':df_analysis_lan_allstar_batting.count()['yearID']
+        }
+    )
+    return df_year_rds
+
+#print df_analysis_lan_batting.plot(kind='bar', stacked=True, color=['red', 'blue'])
+#print pd.concat([df_lan_allstar_batting.count()['H'], df_analysis_lan_batting.count()['AB']], axis=1,
+              #  join_axes=[df_analysis_lan_batting.mean().index])
+    #bulidYearRdsDf(df_analysis_lan_batting)
+
+print bulidYearRdsDf(df_analysis_lan_batting,df_analysis_lan_allstar_batting)
